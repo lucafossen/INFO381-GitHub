dbfdc4aab6e59d1c7258af1584ba9cee8e9e6d1a
OleTP <ot.petrusson@gmail.com>
2025-04-24 14:12:19 +0200

    update utils and rise

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
+
 def get_dataloaders(zip_path="fake_vs_real.zip", local_exctract_dir="../", batch_size=32, transform=None, split="both"):
     """
     Gets train/test DataLoaders (assuming a folder structure similar to the CIFAKE dataset).
@@ -69,3 +70,64 @@ def get_dataloaders(zip_path="fake_vs_real.zip", local_exctract_dir="../", batch
         return train_loader, test_loader
     else:
         raise ValueError("Invalid split. Choose 'train', 'test', or 'both'.")
+
+
+
+
+"""
+Cherry-picked real images for explanation and evaluation.
+
+Descriptions for each image:
+- "1": man with camera - image has black crop
+- "2": car parked in southern Europe
+- "3": man smiling to camera
+- "4": dog laying on grass
+- "5": cat with blue and yellow eyes
+- "6": waterfall from Iceland
+- "7": desert with blue sky
+- "8": model in black and white
+- "9": man staring into the camera
+- "10": painting of man with big ear
+"""
+
+cherry_pick_img_real = {
+    "1": "/content/fake_vs_real/test/real/207.jpg",
+    "2": "/content/fake_vs_real/test/real/212.jpg",
+    "3": "/content/fake_vs_real/test/real/388.jpg",
+    "4": "/content/fake_vs_real/test/real/467.jpg",
+    "5": "/content/fake_vs_real/test/real/653.jpg",
+    "6": "/content/fake_vs_real/test/real/1034.jpg",
+    "7": "/content/fake_vs_real/test/real/1042.jpg",
+    "8": "/content/fake_vs_real/test/real/2028.jpg",
+    "9": "/content/fake_vs_real/test/real/2681.jpg",
+    "10": "/content/fake_vs_real/test/real/eo40kdp8ot5a1.jpg"
+}
+
+
+"""
+Cherry-picked AI-generated (fake) images for explanation and evaluation.
+
+Descriptions for each image:
+- "1": Bull Terrier on man
+- "2": Statue of man's face
+- "3": Girl with skateboard
+- "4": Lady with bear on hike
+- "5": DALLE watermark, girl with phones and phone head
+- "7": Will Smith with gun – seems real but has weird buttons/shadows
+- "8": Fake wolf
+- "9": DALLE watermark, Gender reveal nuclear bomb
+- "10": DALLE watermark, Painting of a king with a burger in his hands
+"""
+
+cherry_pick_img_ai_generated = {
+    "1": "/content/fake_vs_real/test/fake/10098.jpg",
+    "2": "/content/fake_vs_real/test/fake/3158.jpg",
+    "3": "/content/fake_vs_real/test/fake/11729.jpg",
+    "4": "/content/fake_vs_real/test/fake/22314.png",
+    "5": "/content/fake_vs_real/test/fake/12395.jpg",
+    "7": "/content/fake_vs_real/test/fake/22417.png",
+    "8": "/content/fake_vs_real/test/fake/23674.png",
+    "9": "/content/fake_vs_real/test/fake/25755.png",
+    "10": "/content/fake_vs_real/test/fake/12073.jpg"
+}
+
